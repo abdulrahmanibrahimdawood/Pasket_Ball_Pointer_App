3f5afe8f43e1e90cedb05d84afb12c42f3848110
abdulrahmanibrahimdawood <abdoibrahim9111@gmail.com>
2024-10-04 15:12:06 +0300

    finish

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:basket_ball_pointer/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const Pointer());
+    await tester.pumpWidget(Pointer());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
